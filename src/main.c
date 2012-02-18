ce00832fabe4c3f950b5c09be11afdc4821cbe6f
Evgeny Martynov <pro.epic.fail@gmail.com>
2012-02-18 22:41:10 +1100

    Remove some garbage from kmain() printing

--- src/main.c
+++ src/main.c
@@ -17,24 +17,21 @@ int kmain(struct multiboot_t *mboot) {
     }
     monitor_write("Hello, cruel world... :(\n");
     monitor_write("?? O_o you don't see me");
-    monitor_write("\rThis is on the same line!\n");
-    monitor_write("And\t this is tabbled!\t\t!\n");
+    monitor_write("\rThis is on the same line! ");
+    monitor_write("And\tthis is tabbled!\t\t!\n");
     monitor_write_hex(0x0DEFACED);
     monitor_write_dec(1234567890);
-    monitor_write_dec(123);
-    monitor_write_dec(120030);
-    monitor_write_dec(0);
-    monitor_write_dec(123);
     monitor_put(0x08);
     monitor_put('4');
     monitor_put('\n');
 
-    for (i = 5; i < 15; i++) {
-        monitor_write_dec(i);
-        monitor_put('\n');
+    for (i = 1; i <= 10; i++) {
+        printk("%d", i);
+        if (i == 10) printk("!\n");
+        else printk(", ");
     }
 
-    printk(" 123: %d\n4bil: %u\n-123: %d\n", 123, -123, -123);
+    printk("Testing signs\n 123: %d\n4bil: %u\n-123: %d\n", 123, -123, -123);
 
     monitor_write("Reached the end of kmain()\n");
     return 0x00DEFACED;
