6dd7a7b424d6e7ae6986382f3dbbcee0f8880a01
Evgeny Martynov <pro.epic.fail@gmail.com>
2012-02-18 12:18:06 +1100

    Add printing tests to kmain()

--- src/main.c
+++ src/main.c
@@ -7,14 +7,29 @@ int kmain(struct multiboot_t *mboot) {
     int i;
     for (i = 0; i < 80; i++)
         monitor_put(1);
-    monitor_write("Hello, cruel world... :(");
-    monitor_write("?? O_o ");
+    for (i = 0; i < 10; i++) {
+        monitor_write_dec(i*8);
+        monitor_put('\t');
+    }
+    monitor_write("Hello, cruel world... :(\n");
+    monitor_write("?? O_o you don't see me");
+    monitor_write("\rThis is on the same line!\n");
+    monitor_write("And\t this is tabbled!\t\t!\n");
     monitor_write_hex(0x0DEFACED);
     monitor_write_dec(1234567890);
     monitor_write_dec(123);
     monitor_write_dec(120030);
     monitor_write_dec(0);
-    monitor_write_dec(-123);
+    monitor_write_dec(123);
+    monitor_put(0x08);
+    monitor_put('4');
+    monitor_put('\n');
 
+    for (i = 0; i < 20; i++) {
+        monitor_write_dec(i);
+        monitor_put('\n');
+    }
+
+    monitor_write("Reached the end of kmain()\n");
     return 0x00DEFACED;
 }
