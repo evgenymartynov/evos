81cac8a7361a8cdc613a633ae9f7e1082e9b584f
Evgeny Martynov <pro.epic.fail@gmail.com>
2012-02-26 18:39:29 +1100

    Added ordered_array implementation

---
+++ src/ordered_array.c
@@ -0,0 +1,64 @@
+#include "ordered_array.h"
+#include "string.h"
+#include "kmalloc.h"
+#include "panic.h"
+
+static int standard_less_than_cmp(type_t a, type_t b) {
+    return (uint32_t)a < (uint32_t)b;
+}
+
+ordered_array_t ordered_array_create(uint32_t size, less_than_cmp cmp) {
+    return ordered_array_create_at( \
+        (void*)kmalloc(sizeof(type_t) * size), size, cmp);
+}
+
+ordered_array_t ordered_array_create_at(void *where, uint32_t size, \
+    less_than_cmp cmp) {
+    if (!cmp) {
+        cmp = standard_less_than_cmp;
+    }
+
+    ordered_array_t array;
+    array.array = where;
+    memset(array.array, 0, sizeof(type_t) * size);
+    array.comparator = cmp;
+    array.size = 0;
+    array.max_size = size;
+
+    return array;
+}
+
+void ordered_array_insert(ordered_array_t *this, type_t value) {
+    if (this->size + 1 > this->max_size) {
+        panic("Tried to insert into a full ordered_array_t");
+    }
+
+    this->array[this->size++] = value;
+    int i = this->size-1;
+    while (i > 0 && this->comparator(this->array[i], this->array[i]-1)) {
+        type_t temp = this->array[i];
+        this->array[i] = this->array[i-1];
+        this->array[i-1] = temp;
+        i--;
+    }
+}
+
+void ordered_array_remove(ordered_array_t *this, uint32_t index) {
+    if (index >= this->size) {
+        panic("Array index out of bounds");
+    }
+
+    int i = index;
+    this->size--;
+    for (; i < (int)this->size; i++) {
+        this->array[i] = this->array[i+1];
+    }
+}
+
+type_t ordered_array_index(ordered_array_t *this, uint32_t index) {
+    if (index >= this->size) {
+        panic("Array index out of bounds");
+    }
+
+    return this->array[index];
+}
---
+++ src/ordered_array.h
@@ -0,0 +1,23 @@
+#ifndef _ORDERED_ARRAY_H_
+#define _ORDERED_ARRAY_H_
+
+#include "stdint.h"
+
+typedef void *type_t;
+typedef int (*less_than_cmp)(type_t, type_t);
+
+typedef struct {
+    type_t *array;
+    uint32_t size, max_size;
+    less_than_cmp comparator;
+} ordered_array_t;
+
+ordered_array_t ordered_array_create(uint32_t size, less_than_cmp cmp);
+ordered_array_t ordered_array_create_at(void *where, uint32_t size, \
+    less_than_cmp cmp);
+
+void ordered_array_insert(ordered_array_t *this, type_t value);
+void ordered_array_remove(ordered_array_t *this, uint32_t index);
+type_t ordered_array_index(ordered_array_t *this, uint32_t index);
+
+#endif
