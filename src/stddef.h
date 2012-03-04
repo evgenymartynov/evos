b8c7fb166657e7802136b26a84278d3bb5d21bed
Evgeny Martynov <pro.epic.fail@gmail.com>
2012-03-04 18:32:17 +1100

    Moved vnsprintf, va_args, and related shit

--- src/stddef.h
+++ src/stddef.h
@@ -9,4 +9,15 @@
     static int __check_##type __attribute__((unused)) = \
     BUILD_BUG_UNLESS(sizeof(type) == size);
 
+
+typedef char* va_list;
+
+#define __va_stacksize(arg)     \
+    ( ( ( sizeof(arg) + sizeof(int)-1 ) / sizeof(int) ) * sizeof(int) )
+#define va_start(va, prev_arg)  \
+    ( va = (char*)&prev_arg + __va_stacksize(prev_arg) )
+#define va_arg(va, type)        \
+    ( va += __va_stacksize(type),  *(type*)(va - __va_stacksize(type)) )
+#define va_end(va)
+
 #endif
--- src/string.h
+++ src/string.h
@@ -2,11 +2,13 @@
 #define _STRING_H_
 
 #include "stdint.h"
+#include "stddef.h"
 
 void memset(void* ptr, uint8_t value, uint32_t size);
 void memcpy(void *dest, void *src, uint32_t num);
 int is_digit(char c);
 int strlen(const char *string);
 int strcmp(const char *a, const char *b);
+int vsprintf(const char *fmt, char *buf, va_list args);
 
 #endif
